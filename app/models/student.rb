111100e776365c236acbdf60b2af63d939498fdf
rmody3 <rmody3@gmail.com>
2017-02-28 22:58:04 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,17 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def is_active?
+    if self.active
+      <<-HTML
+      <div>"This student is currently active."</div>
+      HTML
+    else
+      <<-HTML
+      <div>"This student is currently inactive."</div>
+      HTML
+    end
+  end
+
 end
